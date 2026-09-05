9af81038896e7e36a3ca15d0a31ee171de094533
t-22-2022 <77286946+t-22-2022@users.noreply.github.com>
2026-09-05 09:49:11 +0800

    Update user agent string in [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -10,7 +10,7 @@ export async function onRequest(context) {
 
   const target = "https://" + path;
 
-  const CUSTOM_UA = "Ott Tv/1.7.3.1 (Linux;Android 12; en; 923k1l)";
+  const CUSTOM_UA = "ott navigator/1.9.6.4 (linux;android 10; en; 11gkvct)";
 
   function random(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
